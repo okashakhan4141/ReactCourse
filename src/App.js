6db902eedfd1f041f9737657c88083cfbdd9200d
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-10-07 18:09:11 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>Todo - APP</h1>
+      <Todo title="React" />
+      <Todo title="Node" />
+    </div>
+  );
+}
+
+export default App;
