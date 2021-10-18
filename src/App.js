dabb75d699349e72f79f6c8426ee2f7cccc275b5
Okasha Khan <71649242+okashakhan4141@users.noreply.github.com>
2021-10-18 17:58:31 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/BL/Users/AddUser";
+import UsersList from "./components/BL/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
